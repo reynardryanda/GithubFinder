29ad6842656d0a9209fe1068f18fe09f6857f87c
ReynardRyanda <reynardryanda@gmail.com>
2019-06-13 20:15:54 +0700

    added client id and client secret

--- github.js
+++ github.js
@@ -1,15 +1,15 @@
 class Github {
   constructor() {
-    this.client_id = "";
-    this.client_secret = "";
+    this.client_id = "ddad575ca0d730ffdeba";
+    this.client_secret = "55d2a5364440c93ef6cfcc728601c85451d3368d";
     this.repos_count = 5;
     this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=&client_secret=`);
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=&client_secret=`);
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
     const repos = await reposResponse.json();
